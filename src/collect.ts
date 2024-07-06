ea5c674b6d7e44c955c796e72aee92f85e77fa8a
Cotton Hou <himcotton@gmail.com>
2024-07-06 09:17:52 +0800

    add collect.ts

---
+++ src/collect.ts
@@ -0,0 +1,50 @@
+import { make_scanner } from './scanner/index.ts';
+import { make_predicate } from './presets/index.ts';
+import { filter, split_by_comma } from './common.ts';
+
+
+
+
+
+export type Flags = Readonly<{
+
+    extra?: ReadonlyArray<string> | undefined,
+
+    ignore?: ReadonlyArray<string> | undefined,
+
+    lodash?: boolean | undefined,
+
+    nolyfill?: boolean | undefined,
+
+}>;
+
+
+
+
+
+export function collect ({ flags, lines }: {
+
+        flags: Flags,
+        lines: AsyncIterable<string>,
+
+}): AsyncIterable<string> {
+
+    const { extra, ignore, ...rest } = flags;
+
+    const pred = make_predicate({
+        extra: extra?.flatMap(split_by_comma),
+        ignore: ignore?.flatMap(split_by_comma),
+        ...rest,
+    });
+
+    //    result = filter pred $ make_scanner 'npm' $ lines
+    const result =                              (
+        filter(pred)                            (
+        make_scanner('npm')                     (
+        lines
+    )));
+
+    return result;
+
+}
+
