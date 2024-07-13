784e075f951f484a48cd07425ac0c099538fcfe1
Cotton Hou <himcotton@gmail.com>
2024-07-13 11:55:28 +0800

    relief toggles in gen_presets

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -4,6 +4,9 @@ export {    lodash };
 import * as nolyfill from './nolyfill.ts';
 export {    nolyfill };
 
+import * as relief from './relief.ts';
+export {    relief };
+
 import type { Predicate } from '../common.ts';
 
 
@@ -15,6 +18,11 @@ export function * gen_presets (param: Partial<Readonly<Record<
         | 'lodash'
         | 'nolyfill'
 
+        | 'relief'
+        | 'relief-native'
+        | 'relief-micro'
+        | 'relief-preferred'
+
 , boolean | undefined>>>): Iterable<Predicate<string>> {
 
     if (param.lodash === true) {
@@ -23,6 +31,30 @@ export function * gen_presets (param: Partial<Readonly<Record<
 
     if (param.nolyfill === true) {
         yield nolyfill.check;
+    }
+
+    if (param.relief === true) {
+
+        yield * [
+            relief.native_check,
+            relief.micro_check,
+            relief.preferred_check,
+        ];
+
+    } else {
+
+        if (param['relief-native'] === true) {
+            yield  relief.native_check;
+        }
+
+        if (param['relief-micro'] === true) {
+            yield  relief.micro_check;
+        }
+
+        if (param['relief-preferred'] === true) {
+            yield  relief.preferred_check;
+        }
+
     }
 
 }
