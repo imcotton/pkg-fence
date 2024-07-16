784b31797f0f201b33774a1521dd661f17bbca74
Cotton Hou <himcotton@gmail.com>
2024-07-16 14:07:52 +0800

    test for deno-info scanner

--- tests/scanner/index.test.ts
+++ tests/scanner/index.test.ts
@@ -1,6 +1,8 @@
 import { describe, it } from '@std/testing/bdd';
 import * as asserts from '@std/assert';
 
+import { make_lines } from '../utils.ts';
+
 import {
 
     make_scanner,
@@ -62,6 +64,62 @@ describe('scanner', function () {
                 'convert-source-map',
                 'acorn-jsx',
                 'buffer',
+            ]);
+
+        });
+
+    });
+
+    describe('deno-info', function () {
+
+        const scanner = make_scanner('deno-info');
+
+        const data = make_lines(`
+dependencies: 15 unique
+size: 619.7KB
+
+npm:/yargs@17.7.2 (285.57KB)
+├─┬ npm:/cliui@8.0.1 (31.56KB)
+│ ├─┬ npm:/string-width@4.2.3 (5.04KB)
+│ │ ├── npm:/emoji-regex@8.0.0 (47.12KB)
+│ │ ├── npm:/is-fullwidth-code-point@3.0.0 (4.88KB)
+│ │ └─┬ npm:/strip-ansi@6.0.1 (3.93KB)
+│ │   └── npm:/ansi-regex@5.0.1 (5.48KB)
+│ ├── npm:/strip-ansi@6.0.1 (3.93KB) *
+│ └─┬ npm:/wrap-ansi@7.0.0 (10.4KB)
+│   ├─┬ npm:/ansi-styles@4.3.0 (16.58KB)
+│   │ └─┬ npm:/color-convert@2.0.1 (26.55KB)
+│   │   └── npm:/color-name@1.1.4 (6.54KB)
+│   ├── npm:/string-width@4.2.3 (5.04KB) *
+│   └── npm:/strip-ansi@6.0.1 (3.93KB) *
+├── npm:/escalade@3.1.2 (11.32KB)
+├── npm:/get-caller-file@2.0.5 (4.61KB)
+├── npm:/require-directory@2.1.1 (11.79KB)
+├── npm:/string-width@4.2.3 (5.04KB) *
+├── npm:/y18n@5.0.8 (22.89KB)
+└── npm:/yargs-parser@21.1.1 (125.46KB)
+        `);
+
+        it('scans from:     deno info npm:yargs', async function () {
+
+            const res = await Array.fromAsync(scanner(data));
+
+            asserts.assertEquals(res, [
+                'cliui',
+                'string-width',
+                'emoji-regex',
+                'is-fullwidth-code-point',
+                'strip-ansi',
+                'ansi-regex',
+                'wrap-ansi',
+                'ansi-styles',
+                'color-convert',
+                'color-name',
+                'escalade',
+                'get-caller-file',
+                'require-directory',
+                'y18n',
+                'yargs-parser',
             ]);
 
         });
