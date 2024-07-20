61dc4aee5290876ccd7c65dba13b3c041f8a619f
Cotton Hou <himcotton@gmail.com>
2024-07-20 09:51:15 +0800

    extract tests of parse out of cli/main

--- tests/cli/main.test.ts
+++ tests/cli/main.test.ts
@@ -8,53 +8,9 @@ import { make_lines } from '../utils.ts';
 
 import {
 
-    parse,
     main,
 
 } from '../../src/cli/main.ts';
-
-
-
-
-
-describe('parse', function () {
-
-    it('supports --format deno-info', function () {
-
-        asserts.assert(
-            'deno-info' === parse([ '--format', 'deno-info' ]).format
-        );
-
-    });
-
-    it('default format to undefined', function () {
-
-        asserts.assert(
-            null == parse([ '--ignore', 'foobar' ]).format
-        );
-
-    });
-
-    it('has format to npm by --format=npm', function () {
-
-        asserts.assert(
-            'npm' === parse([ '--format=npm' ]).format
-        );
-
-        asserts.assert(
-            'npm' === parse([ '--format', 'npm' ]).format
-        );
-
-    });
-
-    it('throws on unknown format', function () {
-
-        asserts.assertThrows(() => parse([ '--format', 'wat' ]));
-        asserts.assertThrows(() => parse([ '--format', 'foobar' ]));
-
-    });
-
-});
 
 
 
