f176f4b22c32d5b8bdb453716e7306633abe0a49
Cotton Hou <himcotton@gmail.com>
2024-08-01 19:12:45 +0800

    upgrade module-replacements@2.3.0

--- src/cli/help.ts
+++ src/cli/help.ts
@@ -21,7 +21,7 @@ Builtin Presets:
 
     --nolyfill                    (via SukkaW/nolyfill v1.0.34)
 
-    --relief                      (via es-tooling/module-replacements v2.2.0)
+    --relief                      (via es-tooling/module-replacements v2.3.0)
         --relief-native
         --relief-micro
         --relief-preferred
